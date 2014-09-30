1f1ba11920bcdae3c66efa55ef4c1b91b95ad4be
zagor3 <zagor3@gmx.de>
2014-09-30 17:25:17 +0200

    Google Map angepasst

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+/* google maps */
+google.maps.visualRefresh = true;
+
+var map;
+function initialize() {
+	var geocoder = new google.maps.Geocoder();
+	var address = $('#map-input').text(); /* change the map-input to your address */
+	var mapOptions = {
+    	zoom: 15,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP,
+     	scrollwheel: false
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	
+  	if (geocoder) {
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+          map.setCenter(results[0].geometry.location);
+
+            var infowindow = new google.maps.InfoWindow(
+                {
+                  content: 'Ristorante Regenbogen',
+                  map: map,
+                  position: results[0].geometry.location
+                });
+
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map, 
+                title:address
+            }); 
+
+          } else {
+          	alert("No results found");
+          }
+        }
+      });
+	}
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+/* end google maps */
+
